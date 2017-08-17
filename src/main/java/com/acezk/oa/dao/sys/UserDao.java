e8f879b1d4a0a657d54e13377fc01940f3aaf509
acezk <acezk@outlook.com>
2017-08-17 21:20:04 +0800

    add query insert update delete method

--- src/main/java/com/acezk/oa/dao/sys/UserDao.java
+++ src/main/java/com/acezk/oa/dao/sys/UserDao.java
@@ -2,10 +2,36 @@ package com.acezk.oa.dao.sys;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.acezk.oa.entity.sys.User;
 
 public interface UserDao {
 	
-	List<User> selectUserList(User u);
+	List<User> queryUserAll(User u);
 	
+	/**
+	 * 通过id查询用户
+	 * @param id
+	 * @return
+	 */
+	User queryUserById(@Param("id") Integer id);
+	
+	/**
+	 * 新增用户
+	 * @param user
+	 */
+	void insertUser(User user);
+	
+	/**
+	 * 修改用户
+	 * @param user
+	 */
+	void updateUser(User user);
+	
+	/**
+	 * 通过id删除用户
+	 * @param id
+	 */
+	void deleteUserById(@Param("id") Integer id);
 }
